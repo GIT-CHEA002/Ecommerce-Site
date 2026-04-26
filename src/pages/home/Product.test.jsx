2fd3c436e68e55515738cf1aaca36f3313aed7ef
Chhun Sokchea <chhunsokchea317@gmail.com>
2026-04-26 14:48:03 +0700

    use beforEach to remove duplication

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, describe, vi, expect } from "vitest";
+import { it, describe, vi, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
@@ -6,24 +6,43 @@ import Product from "./Product";
 vi.mock("axios");
 
 describe("Product", () => {
+  // model prodcut cart (data)
+  let product = {
+    id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
+    image: "images/products/athletic-skateboard-shoes-gray.jpg",
+    name: "Athletic Skateboard Shoes - Gray",
+    rating: {
+      stars: 4,
+      count: 89,
+    },
+    priceCents: 3390,
+    keywords: ["shoes", "running shoes", "footwear"],
+  };
+  /**
+   * Test Hook (prevent bug when we do a testing on our component)
+   * 1. beforEach()
+   * 2. AfterEach()
+   * 3. beforeAll()
+   * 4. afterAll()
+   */
+  let loadCart = vi.fn();
+  beforeEach(() => {
+    product = {
+      id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
+      image: "images/products/athletic-skateboard-shoes-gray.jpg",
+      name: "Athletic Skateboard Shoes - Gray",
+      rating: {
+        stars: 4,
+        count: 89,
+      },
+      priceCents: 3390,
+      keywords: ["shoes", "running shoes", "footwear"],
+    };
+    loadCart = vi.fn();
+  });
+  //=====================Test Detail or product cart ===================
   it("display product detail correctly", () => {
-    const loadCart = vi.fn();
-    render(
-      <Product
-        product={{
-          id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
-          image: "images/products/athletic-skateboard-shoes-gray.jpg",
-          name: "Athletic Skateboard Shoes - Gray",
-          rating: {
-            stars: 4,
-            count: 89,
-          },
-          priceCents: 3390,
-          keywords: ["shoes", "running shoes", "footwear"],
-        }}
-        loadCart={loadCart}
-      />,
-    );
+    render(<Product product={product} loadCart={loadCart} />);
     expect(
       screen.getByText("Athletic Skateboard Shoes - Gray"),
     ).toBeInTheDocument();
@@ -37,24 +56,10 @@ describe("Product", () => {
     );
     expect(screen.getByText("89")).toBeInTheDocument();
   });
+  //========================Test UI(Add To cart button of product component)===================
+  // ui is an async function (to test it we should use await inside async function )
   it("user interaction correctly", async () => {
-    const loadCart = vi.fn();
-    render(
-      <Product
-        product={{
-          id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
-          image: "images/products/athletic-skateboard-shoes-gray.jpg",
-          name: "Athletic Skateboard Shoes - Gray",
-          rating: {
-            stars: 4,
-            count: 89,
-          },
-          priceCents: 3390,
-          keywords: ["shoes", "running shoes", "footwear"],
-        }}
-        loadCart={loadCart}
-      />,
-    );
+    render(<Product product={product} loadCart={loadCart} />);
     const user = userEvent.setup();
     const addToCart = screen.getByTestId("add-to-cart");
     await user.click(addToCart);
